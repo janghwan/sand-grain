6fe2e8c9e360d4021232a53669b87201d4f9409d
Kevin Smithson <ksmithson@pocketly.com>
2014-12-06 19:25:40 -0800

    initial commit for base grain module

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sand Base Grain/Module
+All Grains (Modules) used by Sand must first extend this base grain.
+
+## To begin
+
+ 1. Install it:
+
+    ```bash
+    $ npm install sand-grain -S
+    ```
+    
+## License
+ISC &copy; 2014 Pocketly
---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * Module Dependencies
+ */
+
+var Extend = require('sand-extend').Extend;
+var EventEmitter = require('events').EventEmitter;
+var only = require('only');
+var Logger = require('sand-log');
+var _ = require('lodash');
+
+
+/**
+ * Initialize a new `SandGrain`.
+ *
+ * @api public
+ */
+
+function SandGrain() {
+  this.name = this.name || 'SandGrain';
+  this.configName = this.configName || this.name;
+  this.config = this.config || {};
+  this.defaultConfig = this.defaultConfig || {};
+  this.log = (new Logger(this.name)).log;
+  this.version = this.name == 'SandGrain' ? require('./package').version : 'unknown';
+}
+
+Extend(SandGrain, EventEmitter, {
+  inspect: toJSON,
+
+  init: function(config) {
+    if (typeof config === 'object') {
+      this.config = config;
+    } else if (typeof config === 'string') {
+      // Must be a path
+      this.config = require(config);
+    }
+
+    this.config = _.extend(this.defaultConfig, this.config);
+
+    return this;
+  },
+
+  shutdown: _.noop
+});
+
+function toJSON() {
+  "use strict";
+  return only(this, [
+    'name',
+    'version'
+  ]);
+}
+
+/**
+ * Expose `Application`
+ */
+
+exports = module.exports = SandGrain;
